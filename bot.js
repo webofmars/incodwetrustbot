84b5eef2a0c872afa9b2fc7e73e5d5680f755f4e
Frederic Leger <frederic@webofmars.com>
2016-08-11 11:07:25 +0200

    fixed relic of proxy agent

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ const TelegramBaseController = Telegram.TelegramBaseController
 const tg = new Telegram.Telegram('193218920:AAG9G1zm9K1EFaIHt4HgCwv3AkM0JJozlYA')
 
 var http = require('https')
-var ProxyAgent = require('proxy-agent');
-var proxyUri = process.env.http_proxy || 'http://127.0.0.1:3129';
 
 class PingController extends TelegramBaseController {
     /**
