abb001b354eb83aa343a754454cd2884b86652df
Frederic Leger <frederic@webofmars.com>
2016-08-11 07:55:35 -0400

    start of a skeleton for the bot

--- bot.js
+++ bot.js
@@ -21,5 +21,58 @@ class PingController extends TelegramBaseController {
     }
 }
 
+class DefaultController extends TelegramBaseController {
+   /**
+    * @param {Scope} $
+    */
+    handle($) {
+      console.log('Unknown request received: ')
+      console.log('   Date : ' + $.message.date)
+      console.log('   Id   : ' + $.message.messageId)
+      console.log('   User : ' + $.message.from.username)
+      console.log('   Text : ' + $.message.text)
+      console.log('   Photo: ' + $.message.photo)
+      $.sendMessage('Désolé mais je comprends nib ...');
+    }
+}
+
+class HelpController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    helpHandler($) {
+        $.sendMessage('usage: \
+           /help  : this help \
+           /photo : send us a photo \
+           /ping  : verify bot health')
+    }
+
+    get routes() {
+        return {
+            'help': 'helpHandler'
+        }
+    }
+}
+
+class PhotoController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    photoHandler($) {
+      console.log('received a new photo')
+    }
+
+    get routes() {
+        return {
+            'photo': 'photoHandler'
+        }
+    }
+}
+
+
 tg.router
     .when(['ping'], new PingController())
+    .when(['photo'], new PhotoController())
+    .when(['help'], new HelpController())
+    .when(['start'], new HelpController())
+    .otherwise(new DefaultController())
