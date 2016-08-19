17872f6b2500bb17d6766b94a4810510fbdfe94f
Frederic Leger <frederic@webofmars.com>
2016-08-20 00:10:23 +0200

    more on bot logic

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const tg = new Telegram.Telegram('193218920:AAG9G1zm9K1EFaIHt4HgCwv3AkM0JJozlYA')
-
-var http = require('https')
+const http = require('https')
 
 class PingController extends TelegramBaseController {
     /**
@@ -26,13 +25,22 @@ class DefaultController extends TelegramBaseController {
     * @param {Scope} $
     */
     handle($) {
-      console.log('Unknown request received: ')
-      console.log('   Date : ' + $.message.date)
-      console.log('   Id   : ' + $.message.messageId)
-      console.log('   User : ' + $.message.from.username)
-      console.log('   Text : ' + $.message.text)
-      console.log('   Photo: ' + $.message.photo)
-      $.sendMessage('Désolé mais je comprends nib ...');
+      if ($.message.photo) {
+        console.log('Got a photo !');
+        $.message.photo.forEach(function (item, index, array) {
+          console.log('PhotoSize ' + index + ': ' + item.fileId + '_' + item.width + 'x' + item.height)
+        })
+      }
+      else {
+        console.log('Unknown request received: ')
+        console.log('   Date : ' + $.message.date)
+        console.log('   Id   : ' + $.message.messageId)
+        console.log('   User : ' + $.message.from.username)
+        console.log('   Text : ' + $.message.text)
+        console.log('   Photo: ' + $.message.photo)
+        console.log(' ')
+        $.sendMessage('Désolé mais je comprends nib ...');
+      }
     }
 }
 
@@ -60,6 +68,7 @@ class PhotoController extends TelegramBaseController {
      */
     photoHandler($) {
       console.log('received a new photo')
+      $.message.photo.toJSON()
     }
 
     get routes() {
