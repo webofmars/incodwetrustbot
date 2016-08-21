bf19d081b9592b4e8090b2f92b033b3de66f5161
webofmars <contact@webofmars.com>
2016-08-22 00:39:48 +0200

    Photo logic implemented

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const TGtoken = '193218920:AAG9G1zm9K1EFaIHt4HgCwv3AkM0JJozlYA'
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
-const tg = new Telegram.Telegram('193218920:AAG9G1zm9K1EFaIHt4HgCwv3AkM0JJozlYA')
-const http = require('https')
+const tg = new Telegram.Telegram(TGtoken)
+const dl = require('download-file')
+const dlurl = 'https://api.telegram.org/file/bot'
+const photodir = '/usr/src/app/photos'
 
 class PingController extends TelegramBaseController {
     /**
@@ -25,14 +28,32 @@ class DefaultController extends TelegramBaseController {
     * @param {Scope} $
     */
     handle($) {
+
       if ($.message.photo) {
-        console.log('Got a photo !');
-        $.message.photo.forEach(function (item, index, array) {
-          console.log('PhotoSize ' + index + ': ' + item.fileId + '_' + item.width + 'x' + item.height)
-        })
+        console.log('- Got a photo from ' + $.message.from.username);
+        var daPhoto = $.message.photo[$.message.photo.length-1]
+        console.log('  fileId: ' + daPhoto.fileId)
+        console.log('  width: ' + daPhoto.height)
+        console.log('  width: ' + daPhoto.width)
+        tg.api.getFile(daPhoto.fileId).then(
+          function(daFile) {
+            var url = dlurl + TGtoken + '/' + daFile.filePath
+            var ext = /\..*$/.exec(daFile.filePath)
+            console.log('    Downloading ' + url + '...')
+            dl(url, {directory: photodir + '/' + $.message.from.username, filename: daPhoto.fileId + ext},
+              function(err) {
+                if (err) { throw err; console.log("An error occured: " + JSON.stringify(err))}
+                else {
+                  console.log('    Saved to ' + photodir + '/' + $.message.from.username + '/' + daPhoto.fileId + ext)
+                  console.log('')
+                  $.sendMessage('Merci ' + $.message.from.username)
+                }
+              }
+            )
+          })
       }
       else {
-        console.log('Unknown request received: ')
+        console.log('- Unknown request received: ')
         console.log('   Date : ' + $.message.date)
         console.log('   Id   : ' + $.message.messageId)
         console.log('   User : ' + $.message.from.username)
@@ -57,31 +78,15 @@ class HelpController extends TelegramBaseController {
 
     get routes() {
         return {
-            'help': 'helpHandler'
+            'help': 'helpHandler',
+            'start': 'helpHandler'
         }
     }
 }
-
-class PhotoController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    photoHandler($) {
-      console.log('received a new photo')
-      $.message.photo.toJSON()
-    }
-
-    get routes() {
-        return {
-            'photo': 'photoHandler'
-        }
-    }
-}
-
 
 tg.router
     .when(['ping'], new PingController())
-    .when(['photo'], new PhotoController())
+    .when(['photo'], new DefaultController())
     .when(['help'], new HelpController())
     .when(['start'], new HelpController())
     .otherwise(new DefaultController())
